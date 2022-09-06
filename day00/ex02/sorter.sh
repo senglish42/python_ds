72c11f0295411d4357e4a7ff9324e84e572f3180
Aleksandr <alkhrtnv@github.com>
2022-09-06 15:20:43 +0400

    d00 ex02

--- day00/ex02/sorter.sh
+++ day00/ex02/sorter.sh
@@ -1,2 +1,3 @@
-(head -n -1 ../ex01/hh.csv
-sort -t'"' -k2 ../ex01/hh.csv) > hh_sorted.csv
+#!/bin/bash
+
+(head -n 1 ../ex01/hh.csv; tail -n +2 ../ex01/hh.csv | sort -t',' -k2,2 -k1,1) > hh_sorted.csv
