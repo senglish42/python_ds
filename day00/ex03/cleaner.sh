1c49d532f6f7271c571ea3de5ab6fb35a268fbae
Aleksandr <alkhrtnv@github.com>
2022-09-07 22:59:10 +0400

    d00 ex03

--- day00/ex03/cleaner.sh
+++ day00/ex03/cleaner.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-cut -d"," -f1,2 ../ex02/hh_sorted.csv > hh.csv;
-cut -d"," -f3 < ../ex02/hh_sorted.csv |
+cut -d "," -f1,2 ../ex02/hh_sorted.csv > .tmp1.csv;
+cut -d '"' -f6 ../ex02/hh_sorted.csv |
 sed -e '2,$y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' |\
 sed '2,$s/^/#/' |
 sed '/SENIOR/s/^/Senior\//' |
 sed '/MIDDLE/s/^/Middle\//' |
 sed '/JUNIOR/s/^/Junior\//' |
-sed '/^#/c "-"' |
+sed '/^#/c -' |
 sed -r 's/(Senior\/#).+/\1/' |
 sed -r 's/(Middle\/#).+/\1/' |
 sed -r 's/(Junior\/#).+/\1/' |
 sed 's/\/#.*//g' |
-awk -F"," '{$3; print}' input.csv > output.csv
+sed -r 's/(^|$)/"/g'  > .tmp2.csv ;
+cut -d"," -f 4- ../ex02/hh_sorted.csv |
+sed -e '2,$s/ .*",//g'> .tmp3.csv;
+paste -d "," .tmp1.csv .tmp2.csv .tmp3.csv > hh_positions.csv;
+rm -r .tmp1.csv .tmp2.csv .tmp3.csv
