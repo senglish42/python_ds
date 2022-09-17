71d92f704e60199315b80fd300ed89cb9877d8a4
Aleksandr <alkhrtnv@github.com>
2022-09-18 01:29:18 +0400

    day03 ex03 ex04 temp

--- day03/ex02/librarian.py
+++ day03/ex02/librarian.py
@@ -4,15 +4,17 @@ import os
 class UnconfiguredEnvironment(Exception):
     @staticmethod
     def err_msg():
-        return "Virtual environment name is not valid"
+        return "Virtual environment is not launched or name is not valid"
 
 
 def run():
     try:
-        if not os.getenv("VIRTUAL_ENV").endswith("ex02/senglish"):
+        if not os.getenv("VIRTUAL_ENV") or not os.getenv("VIRTUAL_ENV").endswith("ex02/senglish"):
             raise UnconfiguredEnvironment
         os.system("pip install pytest beautifulsoup4")
         os.system("pip freeze | tee requirements.txt")
+        os.system("tar czf senglish.tar.gz senglish")
+        os.system("rm -rf senglish")
     except UnconfiguredEnvironment as e:
         print(f"Error: {e.err_msg()}")
 
---
+++ day03/ex03/financial.py
@@ -0,0 +1,49 @@
+import time
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+
+def parse(ticker, field):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'}
+    url = f'https://finance.yahoo.com/quote/{ticker}/financials?p={ticker}'
+    try:
+        re = requests.get(url, headers=headers)
+        re.raise_for_status()
+        if "lookup" in re.url:
+            raise Exception("Error: Ticker name is invalid")
+        if "financials" not in re.url:
+            raise Exception(f"Error: Ticker '{ticker}' database can not be downloaded")
+        soup = BeautifulSoup(re.text, 'html.parser')
+        table = soup.find_all("div", attrs={"data-test": 'fin-row'})
+        lst = []
+        for i in table:
+            if len(lst) > 0:
+                break
+            span = i.find_all("span")
+            if span[0].text == field:
+                for j in span:
+                    lst.append(j.text)
+        if len(lst) == 0:
+            print("Error: field of the table is invalid")
+            quit(3)
+        time.sleep(5)
+        return tuple(lst)
+    except requests.RequestException as e:
+        print(e)
+        quit(1)
+    except Exception as e:
+        print(e)
+        quit(2)
+
+
+def run():
+    if len(sys.argv) != 3:
+        print("Error: input two arguments (ticker symbol, the field of the table).")
+        quit(1)
+    print(parse(sys.argv[1].lower(), sys.argv[2]))
+
+
+if __name__ == "__main__":
+    run()
